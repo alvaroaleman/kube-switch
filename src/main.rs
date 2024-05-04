fe64da0d0cc0dfde75d00f1fed1c5dd51f96ae88
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2024-05-04 19:16:57 -0400

    Initial version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use std::env;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::PathBuf;
+use std::process;
+
+use kube::config::Kubeconfig;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Expected exactly one argument, got {}", args.len());
+        process::exit(1)
+    }
+    let new_context = &args[1];
+
+    let mut location = String::new();
+
+    if let Ok(value) = env::var("KUBECONFIG") {
+        if !value.is_empty() {
+            location = value;
+        }
+    } else {
+        let home = env::var("HOME").unwrap();
+        location = PathBuf::from(home)
+            .join(".kube")
+            .join("config")
+            .to_str()
+            .unwrap()
+            .to_string();
+    }
+
+    println!("Kubeconfig location: {}", location);
+
+    let mut kubeconfig_raw = String::new();
+    File::open(&location)
+        .unwrap()
+        .read_to_string(&mut kubeconfig_raw)
+        .unwrap();
+
+    let mut kubeconfig = serde_yaml::from_str::<Kubeconfig>(&kubeconfig_raw).unwrap();
+
+    if kubeconfig
+        .current_context
+        .map(|s| s == *new_context)
+        .unwrap_or(false)
+    {
+        println!("Already in context {}", new_context);
+        process::exit(0);
+    }
+
+    kubeconfig.current_context = Some(new_context.to_string());
+
+    let updated_kubeconfig = serde_yaml::to_string(&kubeconfig).unwrap();
+
+    File::create(location)
+        .unwrap()
+        .write(updated_kubeconfig.as_bytes())
+        .unwrap();
 }
